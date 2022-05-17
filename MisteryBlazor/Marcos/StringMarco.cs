b018d332076e23c054995072f3745e0c7ad31b43
Lam T <portechart@foxmail.com>
2022-05-18 00:00:37 +0800

    font-end

--- MisteryBlazor/Marcos/StringMarco.cs
+++ MisteryBlazor/Marcos/StringMarco.cs
@@ -1,6 +1,6 @@
 ﻿namespace MisteryBlazor.Marcos
 {
-    public static class StringMarco
+    public struct StringMarco
     {
         public const int MAX_STRING_LENGTH = 180;
         public const string CONTEXT_MENU_DIVIDER = "<<hr>>";
--- MisteryBlazor/wwwroot/js/room/room.js
+++ MisteryBlazor/wwwroot/js/room/room.js
@@ -1,9 +1,10 @@
-﻿function ChangeClass(id,className) {
+﻿function ChangeClass(id, className) {
     document.getElementById(id).className = className;
 }
 
 function EnableContextMenu(menuId, handler, activeClass, inactiveClass) {
     const handleElement = document.getElementById(handler);
+    handleElement.classList.add("m-context-menu");
     handleElement.addEventListener("contextmenu", function(event) {
         HandleMenu_contextmenu(event, menuId, activeClass, inactiveClass);
     });
@@ -13,7 +14,7 @@ function EnableContextMenu(menuId, handler, activeClass, inactiveClass) {
 }
 
 function DisableContextMenu(menuId, handler, activeClass, inactiveClass) {
-    const handleElement = document.getElementById(handler);
+    const handleElement = document.getElementById(handler);;
     handleElement.removeEventListener("contextmenu", function (event) {
         HandleMenu_contextmenu(event, menuId, activeClass, inactiveClass);
     });
@@ -25,6 +26,7 @@ function DisableContextMenu(menuId, handler, activeClass, inactiveClass) {
 function HandleMenu_contextmenu(event, menuId, activeClass, inactiveClass) {
     event.preventDefault();
     const contextElement = document.getElementById(menuId);
+    event.stopPropagation();
     contextElement.classList.remove(inactiveClass);
     contextElement.style.top = event.pageY + "px";
     contextElement.style.left = event.pageX + "px";
@@ -33,6 +35,9 @@ function HandleMenu_contextmenu(event, menuId, activeClass, inactiveClass) {
 
 function HandleMenu_onclick(event, menuId, activeClass, inactiveClass) {
     const contextElement = document.getElementById(menuId);
-    contextElement.classList.remove(activeClass);
-    contextElement.classList.add(inactiveClass);
+    if (contextElement) {
+        event.stopPropagation();
+        contextElement.classList.remove(activeClass);
+        contextElement.classList.add(inactiveClass);
+    }
 }
